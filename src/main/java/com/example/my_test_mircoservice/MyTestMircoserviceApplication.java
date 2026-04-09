ac29ea80849dc14fd31c9e353484331bdc4fbe7b
Dheeraj Poonia <dheerajbishnoi123@gmail.com>
2026-04-09 11:38:34 +0530

    first commit

---
+++ src/main/java/com/example/my_test_mircoservice/MyTestMircoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_test_mircoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestMircoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestMircoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/my_test_mircoservice/MyTestMircoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_test_mircoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTestMircoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
